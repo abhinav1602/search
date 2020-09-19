e9223e91a02d1cfb679bbaf5ac24ba4278c899e2
Aditya Tiwari <aditya.tiwari378@gmail.com>
2020-09-19 16:45:41 +0530

    Add files via upload

---
+++ Google Search/images.js
@@ -0,0 +1,8 @@
+function search() {
+  var img = document.getElementById("img").value;
+  location =
+    "https://www.google.co.in/search?q=" +
+    img +
+    "&sxsrf=ALeKk01RLhvuOTJX7XWp0Xf2pZ5tYzfIEA:1600510159262&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiw74H8_PTrAhUiyosBHUY2Dk8Q_AUoAXoECBwQAw&biw=1920&bih=920";
+  return false;
+}
