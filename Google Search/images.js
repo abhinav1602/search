65defa3554bc26be634f7c1e1be1e83e8ca38dc6
Aditya Tiwari <aditya.tiwari378@gmail.com>
2020-09-21 18:09:43 +0530

    LATEST UPDATES

--- Google Search/images.js
+++ Google Search/images.js
@@ -6,3 +6,28 @@ function search() {
     "&sxsrf=ALeKk01RLhvuOTJX7XWp0Xf2pZ5tYzfIEA:1600510159262&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiw74H8_PTrAhUiyosBHUY2Dk8Q_AUoAXoECBwQAw&biw=1920&bih=920";
   return false;
 }
+
+function advanceSearch() {
+  var id1 = document.getElementById("id1").value;
+  var id2 = document.getElementById("id2").value;
+  var id3 = document.getElementById("id3").value;
+  var id4 = document.getElementById("id4").value;
+  location =
+    "https://google.com/search?q=" +
+    id1 +
+    " " +
+    id2 +
+    " " +
+    id3 +
+    " " +
+    "-" +
+    id4;
+  return false;
+}
+
+function lucky() {
+  var val = document.getElementById("luck").value;
+  location =
+    "https://www.google.com/search?&sourceid=navclient&btnI=I&q=" + val;
+  return false;
+}
